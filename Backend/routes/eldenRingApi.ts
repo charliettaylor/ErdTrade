712bb46677d114c65abfd52e564dbb8ad6caf81f
Peekoe <peekoecode@gmail.com>
2022-04-24 22:52:35 -0700

    Error handling, logging, and new endpoints for ER API

--- Backend/routes/eldenRingApi.ts
+++ Backend/routes/eldenRingApi.ts
@@ -1,28 +1,81 @@
 import axios, { AxiosInstance } from 'axios';
 import logging from '../utils/logging';
-import { gql, GraphQLClient, request } from 'graphql-request';
+import { gql, GraphQLClient, request, Variables } from 'graphql-request';
 
 const NAMESPACE: string = "ERAPI";
 
-interface Item {
-    id: string,
-    name: string,
-    image: string,
-    description: string,
-    type: string,
-    effect: string
-}
-
 export default class ERApiController {
 
-    private api: AxiosInstance = axios.create({
-        baseURL: 'https://eldenring.fanapis.com/api/'
-    });
+    private client: GraphQLClient = new GraphQLClient('https://eldenring.fanapis.com/api/graphql');
 
-    public async searchItem(name: string) {
+    private searchQuery(route: string, name: string) : string {
+        return gql`
+        query get {
+            ${route}(search: ${name}) {
+                id,
+                name,
+                image,
+                description
+            }
+        }`;
+    }
+
+    private async makeSearchRequest(route: string, param: string) {
+        try {
+            logging.info(NAMESPACE, `Request made on ${route} for ${param}`);
+            return await this.client.request(this.searchQuery(route, param));
+        } catch (error) {
+            logging.error(NAMESPACE, `Could not search for ${param} in ${route}`);
+            return { error: `Could not search for ${param}` };
+        }
+    }
+
+    private async makeIdRequest(route: string, query: string) {
+        try {
+            logging.info(NAMESPACE, `Request by ID made on ${route}`);
+            return await this.client.request(query);
+        } catch (error) {
+            logging.error(NAMESPACE, `Could not get ${route} by id`);
+            return { error: `Could not get ${route} by id` };
+        }
+    }
+
+    public async searchArmors(name: string) {
+        return await this.makeSearchRequest('armor', name);
+    }
+
+    public async getArmorsById(id: string) {
         let query = gql`
         query get {
-            item(search: ${name}) {
+            armor(id: "${id}") {
+                id,
+                name,
+                image,
+                description,
+                category,
+                weight,
+                dmgNegation{
+                    name,
+                    amount
+                },
+                resistance{
+                    name,
+                    amount
+                }
+            }
+        }`;
+
+        return await this.makeIdRequest('armor', query);
+    }
+
+    public async searchItem(name: string) {
+        return await this.makeSearchRequest('item', name);
+    }
+
+    public async getItemById(id: string) {
+        let query = gql`
+        query get {
+            item(id: "${id}") {
                 id,
                 name,
                 image,
@@ -32,25 +85,79 @@ export default class ERApiController {
             }
         }`;
 
-        const graphQLClient = new GraphQLClient('https://eldenring.fanapis.com/api/graphql');
-        const result = await graphQLClient.request(query);
-        return result;
+        return await this.makeIdRequest('item', query);
     }
 
-    public async getItemById(id: string) {
+    public async searchShields(name: string){
+        return await this.makeSearchRequest('shield', name);
+    }
+
+    public async getShieldsById(id: string) {
         let query = gql`
         query get {
-            item(id: "${id}") {
+            shield(id: "${id}") {
+                id,
                 name,
                 image,
                 description,
-                type,
-                effect
+                category,
+                weight,
+                attack{
+                    name,
+                    amount
+                },
+                defence{
+                    name,
+                    amount
+                },
+                requiredAttributes{
+                    name,
+                    amount
+                },
+                scalesWith{
+                    name,
+                    scaling
+                }
             }
         }`;
 
-        const graphQLClient = new GraphQLClient('https://eldenring.fanapis.com/api/graphql');
-        const result = await graphQLClient.request(query);
-        return result;
+        return await this.makeIdRequest('shield', query);
     }
+
+    public async searchWeapons(name: string){
+        return await this.makeSearchRequest('weapon', name);
+    }
+
+    public async getWeaponsById(id: string) {
+        let query = gql`
+        query get {
+            weapon(id: "${id}") {
+                id,
+                name,
+                image,
+                description,
+                category,
+                weight,
+                attack{
+                    name,
+                    amount
+                },
+                defence{
+                    name,
+                    amount
+                },
+                requiredAttributes{
+                    name,
+                    amount
+                },
+                scalesWith{
+                    name,
+                    scaling
+                }
+            }
+        }`;
+
+        return await this.makeIdRequest('weapon', query);
+    }
+
 }
--- Backend/routes/routes.ts
+++ Backend/routes/routes.ts
@@ -3,16 +3,71 @@ import eldenRingController from './eldenRingApi';
 
 const router = express.Router();
 
+let apiController = new eldenRingController();
+
+router.get('/armors', async (req, res) => {
+    let result = await apiController.searchArmors(req.query.name as string);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
+router.get('/armors/:id', async (req, res) => {
+    let result = await apiController.getArmorsById(req.params.id);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
 router.get('/items', async (req, res) => {
-    let apiController = new eldenRingController();
     let result = await apiController.searchItem(req.query.name as string);
-    res.send(result);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
 });
 
 router.get('/items/:id', async (req, res) => {
-    let apiController = new eldenRingController();
     let result = await apiController.getItemById(req.params.id);
-    res.send(result);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
 });
+
+router.get('/shields', async (req, res) => {
+    let result = await apiController.searchShields(req.query.name as string);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
+router.get('/shields/:id', async (req, res) => {
+    let result = await apiController.getShieldsById(req.params.id);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
+router.get('/weapons', async (req, res) => {
+    let result = await apiController.searchWeapons(req.query.name as string);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
+router.get('/weapons/:id', async (req, res) => {
+    let result = await apiController.getWeaponsById(req.params.id);
+    if(result.error != undefined){
+        return res.status(400).send(result);
+    }
+    return res.send(result);
+});
+
 
 export default router;
