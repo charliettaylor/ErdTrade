905e338a9da18f813a969f2ab3e51f2d53d2d377
sanctumlysis <94922954+0xKoichi@users.noreply.github.com>
2022-04-26 16:48:42 +0100

    Lint

--- Backend/routes/routes.ts
+++ Backend/routes/routes.ts
@@ -1,73 +1,72 @@
 import express from 'express';
-import eldenRingController from './eldenRingApi';
+import EldenRingController from './eldenRingApi';
 
 const router = express.Router();
 
-let apiController = new eldenRingController();
+const apiController = new EldenRingController();
 
 router.get('/armors', async (req, res) => {
-    let result = await apiController.searchArmors(req.query.name as string);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.searchArmors(req.query.name as string);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/armors/:id', async (req, res) => {
-    let result = await apiController.getArmorsById(req.params.id);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.getArmorsById(req.params.id);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/items', async (req, res) => {
-    let result = await apiController.searchItem(req.query.name as string);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.searchItem(req.query.name as string);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/items/:id', async (req, res) => {
-    let result = await apiController.getItemById(req.params.id);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.getItemById(req.params.id);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/shields', async (req, res) => {
-    let result = await apiController.searchShields(req.query.name as string);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.searchShields(req.query.name as string);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/shields/:id', async (req, res) => {
-    let result = await apiController.getShieldsById(req.params.id);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.getShieldsById(req.params.id);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/weapons', async (req, res) => {
-    let result = await apiController.searchWeapons(req.query.name as string);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.searchWeapons(req.query.name as string);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
 
 router.get('/weapons/:id', async (req, res) => {
-    let result = await apiController.getWeaponsById(req.params.id);
-    if(result.error != undefined){
-        return res.status(400).send(result);
-    }
-    return res.send(result);
+  const result = await apiController.getWeaponsById(req.params.id);
+  if (result.error !== undefined) {
+    return res.status(400).send(result);
+  }
+  return res.send(result);
 });
-
 
 export default router;
