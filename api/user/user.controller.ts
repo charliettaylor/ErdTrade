7a0789daefc57171192359f55259f834eae39540
Peekoe <peekoecode@gmail.com>
2022-05-03 23:25:06 -0700

    Add talisman routes, remove unused import

--- api/user/user.controller.ts
+++ api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import UserService from './user.service';
 import { ApiParam, ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
 
