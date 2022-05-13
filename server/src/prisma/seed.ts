257331280e904faab5e88e982858a97353b1a00c
alsheikhiu <alsheikh@iu.edu>
2022-05-12 21:57:19 -0400

    Removed accidental dependencies, testing example in seed.ts

--- server/src/prisma/seed.ts
+++ server/src/prisma/seed.ts
@@ -1,14 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    console.log("Seeding!");
+  console.log('Seeding!');
+
+  await prisma.ammos.create({
+    data: {
+      id: '17f69448ceel0i0a57bokoqz409yb',
+      name: 'Firebone Arrow',
+      image:
+        'https://eldenring.fanapis.com/images/ammos/17f69448ceel0i0a57bokoqz409yb.png',
+      description:
+        'Arrow whittled from animal bones. The tip is set alight before firing',
+      type: 'Pierce',
+      passive: 'Causes blood loss build up (55)',
+    },
+  });
 }
 
-main().catch(e => {
+main()
+  .catch((e) => {
     console.error(e);
     process.exit(1);
-}).finally(async () => {
+  })
+  .finally(async () => {
     await prisma.$disconnect();
-})
+  });
