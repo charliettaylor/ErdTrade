791df5d9cd75d2760de238a3a3b9a4283d61b72e
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-05-22 13:23:43 +0100

    implement server-side sessions

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,9 @@ import {
 } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import expressSession from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import * as CONSTANTS from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -31,6 +34,22 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, customOptions);
+
+  app.use(
+    expressSession({
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+      },
+      secret: CONSTANTS.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(prismaService, {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    }),
+  );
 
   await app.listen(3000);
 }
