12bd3b0f062e886a6af0326c22f68a355cce1253
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-05-22 13:24:00 +0100

    placeholder session secret

--- server/src/common/constants.ts
+++ server/src/common/constants.ts
@@ -4,3 +4,4 @@ export const JWT_ACCESS_TOKEN_SECRET = '@WGu37?<uf(6g`vB@Ufg>Z3%G;qwZ@';
 export const JWT_ACCESS_TOKEN_EXPIRY = 60;
 export const JWT_REFRESH_TOKEN_SECRET = "5CDK;fs:=!{tUG+5SMyYp!T'{?<RU9";
 export const JWT_REFRESH_TOKEN_EXPIRY = 43200;
+export const SESSION_SECRET = 'jzyNHb6DR5YUWo42dXZ4sZ0p0akMRFSA';
