49152807a13cfbe49779d2b69a5f7a3b908b44ad
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-05-23 08:56:04 +0100

    Implement rate-limiting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,11 +7,25 @@ import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import ERApiController from './erapi.controller';
 import { ArmorsModule } from './armors/armors.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ArmorsModule, HealthModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ArmorsModule,
+    HealthModule,
+    ThrottlerModule.forRoot({ ttl: 60, limit: 10 }),
+  ],
   controllers: [AppController, ERApiController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-
 export class AppModule {}
