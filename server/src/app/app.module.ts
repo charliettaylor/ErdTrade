17e576fdcd0fd2de00b520b1d4f2064cd8f1a44f
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-05-24 19:42:38 +0100

    Implement dynamic PrismaModule

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -7,16 +7,33 @@ import { AuthModule } from '../auth/auth.module';
 import { HealthModule } from '../health/health.module';
 import ERApiController from '../erapi/erapi.controller';
 import { EldenRingModule } from '../erapi/erapi.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          datasources: {
+            db: {
+              url: configService.get('DATABASE_URL'),
+            },
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     AuthModule,
     EldenRingModule,
     HealthModule,
-    PrismaModule,
     UserModule,
   ],
   controllers: [AppController, ERApiController],
-  providers: [AppService],
+  providers: [AppService, Logger],
+  exports: [Logger],
 })
 export class AppModule {}
